9542604dbab8ddaf80d322f63d399d9dc03f8591
Simone Canevarolo <110185047+canevarolo@users.noreply.github.com>
2024-12-31 16:12:08 +0100

    Create Laib_10_1.m

---
+++ Laib_10/Laib_10_1.m
@@ -0,0 +1,15 @@
+% An industrial furnace is supported by a brick column with a cross-section of 1 m × 1 m.
+% During steady-state operation, the furnace setup ensures that three faces of the column are maintained at 500 K,
+% while the remaining face is exposed to an air stream at 300 K, with a heat transfer coefficient of h = 10 W/m²K.
+% Determine the temperature map across the column's cross-section and the heat power dissipated into the air per unit length of the column.
+% Density = 1920 kg/m^3; Thermal conductivity = 0.72 W/m/K; Specific heat = 835 J/kg/K
+
+% Laib 10, exercise 1
+% Simone Canevarolo
+% 31/12/2024
+% S269893
+
+clear all
+close all
+clc
+
