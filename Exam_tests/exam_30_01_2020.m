7103fb0c989c5e967a5e8626f157ab2d69a797f6
Simone Canevarolo <110185047+canevarolo@users.noreply.github.com>
2025-01-04 22:50:45 +0100

    Update exam_30_01_2020.m

--- Exam_tests/exam_30_01_2020.m
+++ Exam_tests/exam_30_01_2020.m
@@ -1,3 +1,7 @@
+%% VEDERE FUNZIONI SE SONO GIUSTE, ERRORE NEL GRAFICO RADIALE
+
+
+
 % To study an innovative industrial process, a solid object made of a homogeneous material and shaped like a cylinder is constructed and tested in an
 % induction furnace.
 % The cylinder is, to a first approximation, cooled on all surfaces by an inert gas at a constant temperature of 20 °C with a heat transfer coefficient
@@ -80,4 +84,50 @@ bb(end) = Tgas*hh*dz/kk;
 TT = AA\bb;
 
 figure(1)
-plot(zz,TT)
+plot(zz,TT_ax,'b','LineWidth',2)
+title('Temperature along z axis')
+xlabel('height [m]')
+ylabel('Temperature [K]')
+
+
+% Radius solution
+
+dr = 1e-3; % m
+rr = (0:dr:RR)';
+Nr = length(rr);
+
+As_rad = 2*pi*RR^2;
+
+sub_diag = kk.*(1-dr./rr/2);
+main_diag = (-2*kk-hh*As_rad/Vol)*ones(Nr,1);
+sup_diag = kk.*(1+dr./rr/2);
+
+Band = [[sub_diag(2:end);0], main_diag, [0;sup_diag(1:end-1)]];
+
+AA = spdiags(Band,-1:1,Nr,Nr);
+
+qfunz_rad = @(r) aa.*(1-(r/RR)^2);
+
+jj = 0;
+radspace = 0;
+bb = zeros(Nr,1);
+
+for jj = 1:Nr
+
+    radspace = dr*jj;
+    bb(jj) = -qfunz_rad(radspace)-Tgas*hh*As_rad/Vol;
+
+end
+
+AA(1,1) = -1;
+AA(1,2) = 1;
+bb(1) = 0;
+
+AA(end,end-1) = -1;
+AA(end,end) = 1+hh*dr/kk;
+bb(end) = Tgas*hh*dr/kk;
+
+TT = AA\bb;
+
+figure(2)
+plot(rr,TT)
