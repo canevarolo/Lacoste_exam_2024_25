80c0bad2f5e961ef0ff72cedc3900d14a6ee5073
Simone Canevarolo <110185047+canevarolo@users.noreply.github.com>
2025-01-04 23:22:57 +0100

    Create Laib_10_2.m

---
+++ Laib_10/Laib_10_2.m
@@ -0,0 +1,92 @@
+% Terminare
+
+
+% Laib 10, exercise 2
+% Simone Canevarolo
+% S269893
+% 04/01/2025
+
+clear all
+close all
+clc
+
+alt = 0.1; % m
+lungh = 0.2; % m
+base = 0.1; % m
+alt1 = 0.05; % m
+
+rovol = 7800; % kg/m^3
+kk = 1; % W/m/K
+cp = 450; % J/kg/K
+
+Tin = 20+273; % K
+Tout = 5+273; % K
+
+dx = 1e-4; % m
+dy = dx;
+
+xx = (0:dx:alt)';
+yy = (0:dy:lungh)';
+xx1 = (0:dx:alt1)';
+yy1 = (0:dy:base)';
+
+Nx = length(xx);
+Ny = length(yy);
+Nalt1 = length(xx1);
+Nybase = length(yy1);
+
+Nleft = Nx*Nybase;
+Nrightup = Nleft/2;
+Nrightdown = Nrightup;
+
+Ntot = Nx*Ny;
+
+% Left rectangle
+
+for ii = 2:Nybase-1
+    for jj = 2:Nx-1
+
+        kk = (jj-1)*Nx+ii;
+        AA(kk,kk-Nx) = 1/dy^2;
+        AA(kk,kk-1) = 1/dx^2;
+        AA(kk,kk) = -2*(1/dx^2+1/dy^2);
+        AA(kk,kk+1) = 1/dx^2;
+        AA(kk,kk+Nx) = 1/dy^2;
+
+    end
+end
+
+% Right up rectangle
+
+for ii = 2:Nalt1-1
+    for jj = Nbase+2:Ny-1
+
+        kk = (jj-1)*Nx+ii;
+        AA(kk,kk-Nx) = 1/dy^2;
+        AA(kk,kk-1) = 1/dx^2;
+        AA(kk,kk) = -2*(1/dx^2+1/dy^2);
+        AA(kk,kk+1) = 1/dx^2;
+        AA(kk,kk+Nx) = 1/dy^2;
+
+    end
+end
+
+% Right down rectangle
+
+for ii = Nalt1+2:Nx-1
+    for jj = Nbase+2:Ny-1
+
+        kk = (jj-1)*Nx+ii;
+        AA(kk,kk-Nx) = 1/dy^2;
+        AA(kk,kk-1) = 1/dx^2;
+        AA(kk,kk) = -2*(1/dx^2+1/dy^2);
+        AA(kk,kk+1) = 1/dx^2;
+        AA(kk,kk+Nx) = 1/dy^2;
+
+    end
+end
+
+
+bb = zeros(Ntot,1);
+
+
