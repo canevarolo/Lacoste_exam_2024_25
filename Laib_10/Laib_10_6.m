d458533e321cf492b6f0d18913f9aeebab4cabd6
Simone Canevarolo <110185047+canevarolo@users.noreply.github.com>
2025-01-09 11:42:42 +0100

    Create Laib_10_6.m

---
+++ Laib_10/Laib_10_6.m
@@ -0,0 +1,21 @@
+% A straight fin with a constant cross-section is made of a material with thermal conductivity k = 50 W/m/K,
+% thickness w = 6 mm, and length L = 48 mm. It is very long in the direction perpendicular to the figure.
+% The convective heat transfer coefficient is h = 500 W/m²/K, and the ambient air temperature is Ta = 30°C.
+% The base of the fin is maintained at a constant temperature of Tb = 100°C, while the tip is perfectly insulated.
+
+% a. Using the finite difference method, estimate the temperature distribution along the fin.
+% Is the assumption of one-dimensional heat transfer reasonable for this fin?
+
+% b. Estimate the heat dissipated by the fin per unit length normal to the figure [W/m].
+
+% c. Using the mesh from part (a), calculate and plot the temperature distribution for h = 10, 100, 500, and 1000 W/m²/K.
+
+% Laib 10, exercise 5
+% Simone Canevarolo
+% S269893
+% 09/01/2025
+
+clear all
+close all
+clc
+
