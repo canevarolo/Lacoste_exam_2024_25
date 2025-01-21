7bea5e623bcd942951162bff5ac920a000e819b3
Simone Canevarolo <110185047+canevarolo@users.noreply.github.com>
2025-01-21 12:18:34 +0100

    Update and rename ecam_june_2019.m to exam_june_2019.m

--- Exam_tests/exam_june_2019.m
+++ Exam_tests/exam_june_2019.m
@@ -25,6 +25,8 @@
 % S269893
 % Exam test - June 2019
 
+% TRANSIENT
+
 clear all
 close all
 clc
@@ -57,7 +59,8 @@ kkguaina = 16.3; % W/m/K
 roguaina = 6753; % kg/m^3
 cpguaina = 327; % J/kg/K
 
-dr = 1e-5; % m
+
+dr = 1e-4; % m
 rr = (0:dr:raggio)';
 rr1 = (0:dr:rcomb)';
 rr2 = (rcomb:dr:raggio)';
@@ -66,8 +69,55 @@ Nr = length(rr);
 Nr1 = length(rr1);
 Nr2 = length(rr2);
 
+aa1 = kkmox/dr^2;
+aa2 = kkguaina/dr^2;
+
+
+sub_diag1 = -aa1.*(1-dr/2./rr1);
+main_diag1 = (1+2*aa1)*ones(Nr1,1);
+sup_diag1 = -aa1.*(1+dr/2./rr1);
+
+sub_diag2 = -aa2.*(1-dr/2./rr2);
+main_diag2 = (1+2*aa2)*ones(Nr2,1);
+sup_diag2 = -aa2.*(1+dr/2./rr2);
+
+sub_diag = [sub_diag1; sub_diag2];
+main_diag = [main_diag1; main_diag2];
+sup_diag = [sup_diag1; sup_diag2];
+
+Band = [[sub_diag(2:end);0], main_diag, [0;sup_diag(1:end-1)]];
+
+AA = spdiags(Band,-1:1,Nr,Nr);
+
+bb = zeros(Nr,1);
+bb(1:Nr1) = qvolzero;
+% bb(Nr1+1:Nr) = 0;
+
+AA(1,1) = 1;
+AA(1,2) = -1;
+bb(1) = 0;
+
+AA(Nr1,Nr1-1) = kkguaina;
+AA(Nr1,Nr1) = -kkguaina-kkmox;
+AA(Nr1,Nr1+1) = kkmox;
+bb(Nr1) = 0;
+
+AA(end,end-1) = -kkguaina/dr;
+AA(end,end) = kkguaina/dr+hh;
+bb(end) = Tna*hh;
+
+TT = AA\bb;
+
+figure(1)
+plot(rr,TT-273)
+
+% STEADY-STATE
+
 dt = 0.1; % s
 time = 0; % s
+
+aa1 = kkmox*dt/romox/cpmox/dr^2;
+aa2 = kkguaina*dt/roguaina/cpguaina/dr^2;
 
 Tm = Tna*ones(Nr,1);
 Tm1 = Tna*ones(Nr1,1);
@@ -75,11 +125,9 @@ Tm2 = Tna*ones(Nr2,1);
 
 Tmax = Tna;
 
+
 while Tmax < Tend
 
-    aa1 = kkmox*dt/romox/cpmox/dr^2;
-    aa2 = kkguaina*dt/roguaina/cpguaina/dr^2;
-    
     sub_diag1 = -aa1.*(1-dr/2./rr1);
     main_diag1 = (1+2*aa1)*ones(Nr1,1);
     sup_diag1 = -aa1.*(1+dr/2./rr1);
@@ -91,7 +139,6 @@ while Tmax < Tend
     sub_diag = [sub_diag1; sub_diag2];
     main_diag = [main_diag1; main_diag2];
     sup_diag = [sup_diag1; sup_diag2];
-    
     
     Band = [[sub_diag(2:end);0], main_diag, [0;sup_diag(1:end-1)]];
     
@@ -123,5 +170,5 @@ while Tmax < Tend
 
 end
 
-figure(1)
+figure(2)
 plot(rr,TT-273,'LineWidth',2)
